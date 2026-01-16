b586489cea3509b15b6713072a029dcdfb6a4b85
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 05:37:33 -0600

    2026-01-16 05:35 I believe the remapping of packages from server.markhome.mcf to io.github.msobkow is complete.

---
+++ README.md
@@ -0,0 +1,28 @@
+io.github.msobkow.v3_1.cfsec.cfsecsaxramldr
+
+Copyright (c) 2016-2026 Mark Stephen Sobkow
+
+Mark's Code Fractal 3.1 CFSec - Security Services
+
+These files are part of Mark's Code Fractal CFSec.
+
+Mark's Code Fractal CFSec is available under dual commercial license from
+Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+Version 3 or later.
+
+Mark's Code Fractal CFSec is free software: you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Mark's Code Fractal CFSec is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with Mark's Code Fractal CFSec.  If not, see <https://www.gnu.org/licenses/>.
+
+If you wish to modify and use this code without publishing your changes in order to
+tie it to proprietary code, please contact Mark Stephen Sobkow
+for a commercial license at mark.sobkow@gmail.com
---
+++ src/main/java/io/github/msobkow/v3_1/cfsec/cfsecsaxramldr/CFSecSaxRamLdr.java
@@ -0,0 +1,134 @@
+// Description: Java 25 XML SAX Parser for CFSec.
+
+/*
+ *	io.github.msobkow.CFSec
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal 3.1 CFSec - Security Services
+ *	
+ *	This file is part of Mark's Code Fractal CFSec.
+ *	
+ *	Mark's Code Fractal CFSec is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later.
+ *	
+ *	Mark's Code Fractal CFSec is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFSec is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFSec.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ *	
+ */
+
+package io.github.msobkow.v3_1.cfsec.cfsecsaxramldr;
+
+import org.apache.log4j.*;
+import io.github.msobkow.v3_1.cflib.*;
+import io.github.msobkow.v3_1.cflib.xml.*;
+import io.github.msobkow.v3_1.cfsec.cfsec.*;
+
+import io.github.msobkow.v3_1.cfsec.cfsecobj.*;
+import io.github.msobkow.v3_1.cfsec.CFSecRam.*;
+import io.github.msobkow.v3_1.cfsec.CFSecSaxLoader.*;
+
+public class CFSecSaxRamLdr
+	extends CFSecSaxLdr
+{
+	private static ICFLibMessageLog log = new CFLibConsoleMessageLog();
+
+	// Constructors
+
+	public CFSecSaxRamLdr() {
+		super( log );
+	}
+
+	// main() entry point
+
+	public static void main( String args[] ) {
+		final String S_ProcName = "CFSecSaxRamLdr.main() ";
+		initConsoleLog();
+		int numArgs = args.length;
+		if( numArgs >= 2 ) {
+			ICFSecSchema cFSecSchema = new CFSecRamSchema();
+			ICFSecSchemaObj cFSecSchemaObj = new CFSecSchemaObj();
+			cFSecSchemaObj.setBackingStore( cFSecSchema );
+			CFSecSaxLdr cli = new CFSecSaxRamLdr();
+			CFSecSaxLoader loader = cli.getSaxLoader();
+			loader.setSchemaObj( cFSecSchemaObj );
+			String url = args[1];
+			try {
+				cFSecSchema.connect( "system", "system", "system", "system" );
+				cFSecSchema.rollback();
+				cFSecSchema.beginTransaction();
+				cFSecSchemaObj.setSecCluster( cFSecSchemaObj.getClusterTableObj().getSystemCluster() );
+				cFSecSchemaObj.setSecTenant( cFSecSchemaObj.getTenantTableObj().getSystemTenant() );
+				cFSecSchemaObj.setSecSession( cFSecSchemaObj.getSecSessionTableObj().getSystemSession() );
+				CFSecAuthorization auth = new CFSecAuthorization();
+				auth.setSecCluster( cFSecSchemaObj.getSecCluster() );
+				auth.setSecTenant( cFSecSchemaObj.getSecTenant() );
+				auth.setSecSession( cFSecSchemaObj.getSecSession() );
+				cFSecSchemaObj.setAuthorization( auth );
+				loader.setUseCluster( cFSecSchemaObj.getSecCluster() );
+				loader.setUseTenant( cFSecSchemaObj.getSecTenant() );
+				applyLoaderOptions( loader, args[0] );
+				cli.evaluateRemainingArgs( args, 2 );
+				loader.parseFile( url );
+				cFSecSchema.commit();
+				cFSecSchema.disconnect( true );
+			}
+			catch( Exception e ) {
+				log.message( S_ProcName + "EXCEPTION: Could not parse XML file \"" + url + "\": " + e.getMessage() );
+				e.printStackTrace( System.out );
+			}
+			catch( Error e ) {
+				log.message( S_ProcName + "ERROR: Could not parse XML file \"" + url + "\": " + e.getMessage() );
+				e.printStackTrace( System.out );
+			}
+			finally {
+				if( cFSecSchema.isConnected() ) {
+					cFSecSchema.rollback();
+					cFSecSchema.disconnect( false );
+				}
+			}
+		}
+		else {
+			log.message( S_ProcName + "ERROR: Expected at least two argument specifying the loader options and the name of the XML file to parse.  The first argument may be empty." );
+		}
+	}
+
+	// Initialize the console log
+
+	protected static void initConsoleLog() {
+//		Layout layout = new PatternLayout(
+//				"%d{ISO8601}"		// Start with a timestamp
+//			+	" %-5p"				// Include the severity
+//			+	" %C.%M"			// pkg.class.method()
+//			+	" %F[%L]"			// File[lineNumber]
+//			+	": %m\n" );			// Message text
+//		BasicConfigurator.configure( new ConsoleAppender( layout, "System.out" ) );
+	}
+
+	// Evaluate remaining arguments
+
+	public void evaluateRemainingArgs( String[] args, int consumed ) {
+		// There are no extra arguments for the RAM "database" instance
+		if( args.length > consumed ) {
+			log.message( "CFSecSaxRamLdr.evaluateRemainingArgs() WARNING No extra arguments are expected for a RAM database instance, but "
+				+ Integer.toString( args.length - consumed )
+				+ " extra arguments were specified.  Extra arguments ignored." );
+		}
+	}
+
+}
